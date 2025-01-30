128c1dda4e0892c9235acfb15b47332de6a99202
Porrith Suong <porrith.suong@gmail.com>
2025-01-29 19:56:03 -0800

    Adds a todo for the output.

--- Assets/UIToolkit.PostProcessor/UIDocumentProcessor.cs
+++ Assets/UIToolkit.PostProcessor/UIDocumentProcessor.cs
@@ -1,5 +1,10 @@
+using NUnit.Framework;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Xml;
 using UnityEditor;
 using UnityEngine;
@@ -7,19 +12,22 @@ using UnityEngine.UIElements;
 
 namespace InitialPrefabs.UIToolkit.PostProcessor {
 
+    class TaskWorker {
+
+    }
+
     public class UIDocumentProcessor : AssetPostprocessor {
         static readonly StringBuilder Builder = new StringBuilder(512);
-        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload) {
+        static readonly List<XmlDocument> Documents = new List<XmlDocument>(5);
+        private static async void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths, bool didDomainReload) {
             Log("IMPORTED: ", importedAssets);
             Log("DELETED: ", deletedAssets);
             Log("MOVED: ", movedAssets);
             Log("MOVED FROM: ", movedFromAssetPaths);
 
-            Span<int> validFiles = stackalloc int[importedAssets.Length];
-            var count = 0;
+            Documents.Clear();
+            LoadXmlDocs(importedAssets, Documents);
 
-            GetValidFileIndices(importedAssets, ref validFiles, ref count);
-            ProcessUXMLFiles(importedAssets, validFiles, count);
         }
 
         private static void GetValidFileIndices(string[] importAssets, ref Span<int> validIndices, ref int count) {
@@ -31,17 +39,56 @@ namespace InitialPrefabs.UIToolkit.PostProcessor {
             }
         }
 
+        private static void LoadXmlDocs(string[] importedAssets, List<XmlDocument> documents) {
+            documents.Clear();
+            foreach (var path in importedAssets) {
+                if (path.SimpleEndsWith(".uxml")) {
+                    var doc = new XmlDocument();
+                    doc.Load(FileUtils.AbsolutePath(path));
+                    documents.Add(doc);
+                    // TODO: Stash some metadata so we know what names will need to be added to a document.
+                }
+            }
+        }
+
+        private static async void PrepareThreadWorkloads(IReadOnlyList<XmlDocument> documents, int threadCount) {
+            var tasks = new Task[threadCount];
+            var sliceSize = documents.Count / threadCount;
+            var span = new (int startIndex, int length)[threadCount];
+
+            for (var i = 0; i < threadCount; i++) {
+                span[i] = (i * sliceSize, i == threadCount - 1 ? documents.Count - i * sliceSize : sliceSize);
+            }
+
+            for (var i = 0; i < threadCount; i++) {
+                tasks[i] = Task.Factory.StartNew(() => {
+                    (var startIndex, var length) = span[i];
+
+                    for (var j = 0; j < length; j++) {
+                        var offset = startIndex + j;
+                        var xml = documents[offset];
+                        Recurse(xml, node => {
+                            foreach (XmlNode attribute in node.Attributes) {
+                                if (attribute.Name == "name") {
+                                    // TODO: Store the value into a map, maybe add a hierarchial output, because I want to generate a source file per XML doc
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
         private static void ProcessUXMLFiles(string[] importedAssets, Span<int> validIndices, int count) {
             for (var i = 0; i < count; i++) {
                 var assetPath = importedAssets[validIndices[i]];
-                Debug.Log(assetPath);
-                Debug.Log(Option<VisualTreeAsset>.Some(AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(assetPath)).IsValid);
                 Option<VisualTreeAsset>.Some(AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(assetPath)).Ok(document => {
                     var xml = new XmlDocument();
-                    FileUtils.AbsolutePath(assetPath);
-                    Debug.Log(FileUtils.AbsolutePath(assetPath));
                     xml.Load(FileUtils.AbsolutePath(assetPath));
 
+                    // TODO: Launch multiple threads on the # of valid files
                     Recurse(xml, node => {
                         Debug.Log($"Attribute for: {node.Name}");
                         foreach (XmlNode attribute in node.Attributes) {
@@ -53,8 +100,7 @@ namespace InitialPrefabs.UIToolkit.PostProcessor {
         }
 
         private static void Recurse(XmlNode node, Action<XmlNode> onNode) {
-            foreach (XmlNode c in node.ChildNodes)
-            {
+            foreach (XmlNode c in node.ChildNodes) {
                 onNode?.Invoke(c);
                 Recurse(c, onNode);
             }
